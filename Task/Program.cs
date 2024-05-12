26ce64f28ff50daa5bd13afa61d2ec4b72150e99
Anastasia Afanasieva <nikolaeva108asya@mail.ru>
2024-05-12 22:49:18 +0300

    Записан цикл

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,3 +7,18 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array1 = new string[5] {"123", "23", "hello", "world", "-2"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
